f6714c0a5059a855083baa1f227a38a81350c878
matheusbsilva <matheusbattista@hotmail.com>
2020-05-21 16:51:10 -0300

    add ifix and dolar to response

--- core/jobs.py
+++ core/jobs.py
@@ -1,6 +1,6 @@
 import logging
 
-from extensions.market_indexes import get_ibovespa, get_sandp
+from extensions.market_indexes import get_ibovespa, get_sandp, get_ifix, get_dolar_to_brl
 
 logger = logger = logging.getLogger(__name__)
 
@@ -11,11 +11,17 @@ def daily_indexes(context):
 
     ibov_time, ibov_price = get_ibovespa()
     sandp_time, sandp_price = get_sandp()
+    ifix_time, ifix_price = get_ifix()
+    dolar_time, dolar_price = get_dolar_to_brl()
 
     message = """
 IBOVESPA: {ibov_price} - {ibov_time}
 S&P 500: {sandp_price} - {sandp_time}
+IFIX: {ifix_price} - {ifix_time}
+Dólar: {dolar_price} - {dolar_time}
     """.format(ibov_price=ibov_price, ibov_time=ibov_time,
-               sandp_price=sandp_price, sandp_time=sandp_time)
+               sandp_price=sandp_price, sandp_time=sandp_time,
+               ifix_price=ifix_price, ifix_time=ifix_time,
+               dolar_price=dolar_price, dolar_time=dolar_time)
 
     context.bot.send_message(chat_id=job.context, text=message)
--- extensions/market_indexes.py
+++ extensions/market_indexes.py
@@ -4,15 +4,21 @@ from datetime import datetime
 import requests
 
 YAHOO_BASE_URL = os.environ.get('YAHOO_BASE_URL')
+BCB_BASE_URL = os.environ.get('BCB_BASE_URL')
 
 
 def get_yahoo_finance(symbol):
+    """
+    Retrieve last price data of the given symbol from
+    Yahoo Finance API
+    """
+
     url = "{}?symbols={}&range=1d&interval=1d&indicators=close".format(YAHOO_BASE_URL, symbol)
 
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception("Error retrieving ibovespa data code: {}".format(response.status_code))
+        raise Exception("Error retrieving ibovespa data. Code: {}".format(response.status_code))
 
     data = response.json()['spark']['result'][0]['response'][0]
 
@@ -22,11 +28,46 @@ def get_yahoo_finance(symbol):
     return timestamp.strftime("%d/%m/%Y - %H:%M"), market_price
 
 
+def get_dolar_to_brl():
+    """
+    Retrieve last dolar to brl quotation from Central Bank of Brazil API
+    """
+
+    dolar_code = 1
+    url = "{}/bcdata.sgs.{}/dados/ultimos/1?formato=json".format(BCB_BASE_URL,
+                                                                 dolar_code)
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise Exception("Error retrieving Dolar data. Code: {}".format(response.status_code))
+
+    data = response.json()[0]
+
+    return data['data'], data['valor']
+
+
 def get_ibovespa():
+    """
+    Retrive IBOVESPA price from Yahoo Finance API
+    """
+
     ibov_symbol = "^BVSP"
     return get_yahoo_finance(ibov_symbol)
 
 
 def get_sandp():
+    """
+    Retrive S&P 500 price from Yahoo Finance API
+    """
+
     sandp_symbol = "^GSPC"
     return get_yahoo_finance(sandp_symbol)
+
+
+def get_ifix():
+    """
+    Retrive IFIX price from Yahoo Finance API
+    """
+    ifix_symbol = "IFIX.SA"
+    return get_yahoo_finance(ifix_symbol)
