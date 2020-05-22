c9f60e500a0cf8af3b61da800903f85a8409b39b
matheusbsilva <matheusbattista@hotmail.com>
2020-05-22 20:28:46 -0300

    create database class to save registred chat ids

---
+++ core/database.py
@@ -0,0 +1,41 @@
+import json
+import logging
+
+logger = logger = logging.getLogger(__name__)
+
+
+class Database:
+    """
+    Define Database as a Singleton class to retrieve data
+    """
+
+    _instance = None
+
+    def __new__(cls, filepath):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.filepath = filepath
+
+        return cls._instance
+
+    def data(self):
+
+        data = {}
+        try:
+            with open(self.filepath, "r") as fp:
+                data = json.loads(fp.read())
+        except FileNotFoundError:
+            logger.warning("File %s does not exist" % self.filepath)
+
+        return data
+
+    def save(self, data):
+        curr_data = self.data()
+
+        try:
+            curr_data['ids'].append(data)
+        except KeyError:
+            curr_data = {"ids": [data]}
+
+        with open(self.filepath, "w") as fp:
+            json.dump(curr_data, fp)
