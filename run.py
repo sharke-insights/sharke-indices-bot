a175a4a08d830078310cbfbc9c5f2208459dd1e2
matheusbsilva <matheusbattista@hotmail.com>
2020-07-10 18:03:42 -0300

    add execution type env var

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 
 from core.bot import run
-from core.envs import TOKEN, WEBHOOK_URL, HOST, PORT
+from core.envs import TOKEN, WEBHOOK_URL, HOST, PORT, EXECUTION_TYPE
 
 if __name__ == '__main__':
-    run(TOKEN, 'webhook', HOST, PORT, WEBHOOK_URL)
+    run(TOKEN, EXECUTION_TYPE, HOST, PORT, WEBHOOK_URL)
 
